a1600ab52e5bd1c50663d4b1ecdb208e16a26836
TheJohny71 <jduval71@me.com>
2025-02-23 03:12:27 +0000

    updates

---
+++ eor/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col p-6">
+      <h1 className="text-4xl font-bold">Executive Order Tracker</h1>
+    </main>
+  )
+}
